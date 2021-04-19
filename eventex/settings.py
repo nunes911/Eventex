8a4ff3f4ebf49c307b92574b53217bb9d84dcb2e
Marcus Nunes <marcus.live.nunes@gmail.com>
2021-04-19 16:31:56 -0300

    Ignora DEFAULT_AUTO_FIELD

--- eventex/settings.py
+++ eventex/settings.py
@@ -127,4 +127,4 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
