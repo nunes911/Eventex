7c692f1cc3b2cbcba88bfc63019ff1e79a152fd9
Marcus Nunes <marcus.live.nunes@gmail.com>
2021-04-22 14:42:23 -0300

    fix database

--- eventex/settings.py
+++ eventex/settings.py
@@ -83,12 +83,11 @@ WSGI_APPLICATION = 'eventex.wsgi.application'
 
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
