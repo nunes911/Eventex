54d32a88b17dc67ee24fc62d7a48db94e4e7c0d0
Marcus Nunes <marcus.live.nunes@gmail.com>
2021-04-26 11:07:53 -0300

    SImplica com modelform

--- eventex/subscriptions/tests/test_view_new.py
+++ eventex/subscriptions/tests/test_view_new.py
@@ -21,7 +21,7 @@ class SubscriptionsNewGet(TestCase):
         """Html must contain input tags"""
         tags = (('<form', 1),
                 ('<input', 6),
-                ('type="text"', 4),
+                ('type="text"', 3),
                 ('type="email"', 1),
                 ('type="submit"', 1))
         for text, count in tags:
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -27,7 +27,7 @@ def create(request):
         return render(request, "subscriptions/subscription_form.html", 
                         {'form': form})
     
-    subscription = Subscription.objects.create(**form.cleaned_data)
+    subscription = form.save()
 
     # Send subscription email
     _send_mail('subscriptions/subscription_email.txt', 
