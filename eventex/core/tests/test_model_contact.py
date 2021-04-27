e190be500ac57d8ad744414cd27cb3b8aa66d494
Marcus Nunes <marcus.live.nunes@gmail.com>
2021-04-27 15:03:38 -0300

    Contatos

--- eventex/core/tests/test_model_contact.py
+++ eventex/core/tests/test_model_contact.py
@@ -1,20 +1,46 @@
 from django.test import TestCase
-from eventex.core.models import Speaker
+from django.core.exceptions import ValidationError
+from eventex.core.models import Speaker, Contact
 
 
-# class ContactModelTest(TestCase):
-#     def test_create(self):
-#         speaker = Speaker.objects.create(
-#             name='Marcus Nunes',
-#             slug='marcus-nunes',
-#             photo='http://hbn.link/hb-pic'
-#         )
+class ContactModelTest(TestCase):
+    def setUp(self):
+        self.speaker = Speaker.objects.create(
+            name='Marcus Nunes',
+            slug='marcus-nunes',
+            photo='http://hbn.link/hb-pic'
+        )
+
+    def test_email(self):
+        contact = Contact.objects.create(
+            speaker=self.speaker,
+            kind=Contact.EMAIL,
+            value='marcus@nunes.net'
+        )
+
+        self.assertTrue(Contact.objects.exists())
     
-#         contact = Contact.objects.create(
-#             speaker=speaker,
-#             kind='E',
-#             value='marcus@nunes.net'
-#         )
+    def test_phone(self):
+        contact = Contact.objects.create(
+            speaker=self.speaker,
+            kind=Contact.PHONE,
+            value='21-987654321'
+        )
 
-#         self.assertTrue(Contact.objects.exists())
+        self.assertTrue(Contact.objects.exists())
+    
+    def test_choices(self):
+        """Contact kind should be limited to E or P"""
+        contact = Contact(speaker=self.speaker, kind='A', value='B')
+
+        self.assertRaises(ValidationError, contact.full_clean)
+    
+    def test_str(self):
+        contact = Contact(
+            speaker=self.speaker,
+            kind=Contact.EMAIL,
+            value='marcus@nunes.net'
+        )
+
+        self.assertEqual('marcus@nunes.net', str(contact))
         
