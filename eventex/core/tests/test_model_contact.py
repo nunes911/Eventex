801374c931b336d840bcb906a9051b1167098909
Marcus Nunes <marcus.live.nunes@gmail.com>
2021-04-27 13:51:40 -0300

    Speaker

---
+++ eventex/core/tests/test_model_contact.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from eventex.core.models import Speaker
+
+
+# class ContactModelTest(TestCase):
+#     def test_create(self):
+#         speaker = Speaker.objects.create(
+#             name='Marcus Nunes',
+#             slug='marcus-nunes',
+#             photo='http://hbn.link/hb-pic'
+#         )
+    
+#         contact = Contact.objects.create(
+#             speaker=speaker,
+#             kind='E',
+#             value='marcus@nunes.net'
+#         )
+
+#         self.assertTrue(Contact.objects.exists())
+        
